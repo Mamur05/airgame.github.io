6fd8451072ed2b7bc75b51e850373b1afbf49673
Mamur05 <100681028+Mamur05@users.noreply.github.com>
2022-03-01 13:26:15 +0300

    Add files via upload

---
+++ ajax/serf.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('FastCore') || FastCore !== true) { Header('Location: /404'); return; }
+if (!isset($_SESSION['uid'])) { exit(); }
+$type = filter_var($_GET['type'],FILTER_SANITIZE_STRING);
+if(file_exists(BASE_DIR.'/ajax/serf/'.$type.'.php')){
+    $uid = $_SESSION['uid'];
+	$login = $_SESSION['login'];
+    $data = array();
+    include(BASE_DIR.'/ajax/serf/'.$type.'.php');
+}else{
+    echo 'Unknown request';
+}
